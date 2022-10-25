f7f4eb2045ca7256cd85d2c197b0838e35ecaeb0
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-10-25 12:15:36 +0900

    배경 이미지 추가

--- src/tya_game1/TyaGame.java
+++ src/tya_game1/TyaGame.java
@@ -6,21 +6,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class TyaGame extends JFrame {
-	private Image background=new ImageIcon(Main.class.getResource("경로")).getImage();//배경이미지
-	public void paint(Graphics g) {//그리는 함수
-		g.drawImage(background, 0, 0, null);//background를 그려줌
+	private Image background;
+	public void paint(Graphics g) {
+		//그리는 함수 
+		g.drawImage(background, 0, 0, null);	//background를 그려줌 
 	}
 	public TyaGame() {
 		setTitle("TYA Game");
-      	setVisible(true);
-      	setResizable(false);//창의 크기를 변경하지 못하게
-      	setExtendedState(JFrame.MAXIMIZED_BOTH);   //2560,1440
-      	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //창을 닫으면 프로세스도 종료
+		setVisible(true);
+		setResizable(false);// 창의 크기를 변경하지 못하게
+		setExtendedState(JFrame.MAXIMIZED_BOTH); // 2560,1440
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로세스도 종료
+		background = new ImageIcon(Main.class.getResource("../images/background.jpg")).getImage();	//배경이미지
 	}
-   
+
 	public static void main(String[] args) {
 		new TyaGame();
-      
 	}
 
 }
