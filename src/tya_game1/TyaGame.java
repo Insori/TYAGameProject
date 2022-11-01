8eec7b2799722f00101b315b209dbe2ed53bd032
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-01 11:19:44 +0900

    main화면 시작 띄우기, 가운데 맞추기

--- src/tya_game1/TyaGame.java
+++ src/tya_game1/TyaGame.java
@@ -6,18 +6,30 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class TyaGame extends JFrame {
-	private Image background;
-	public void paint(Graphics g) {
-		//그리는 함수 
-		g.drawImage(background, 0, 0, null);	//background를 그려줌 
-	}
+	private Image screenImage;
+	private Graphics screenGraphic;
+	private Image background = new ImageIcon(Main.class.getResource("../images/background.jpg")).getImage();	//배경이미지
+	
 	public TyaGame() {
 		setTitle("TYA Game");
-		setVisible(true);
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);// 창의 크기를 변경하지 못하게
-		setExtendedState(JFrame.MAXIMIZED_BOTH); // 2560,1440
+		setLocationRelativeTo(null);	//화면의 위치가 가운데로 옴.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로세스도 종료
-		background = new ImageIcon(Main.class.getResource("../images/background.jpg")).getImage();	//배경이미지
+		setVisible(true);
+		}
+
+	public void paint(Graphics g) {
+		//그리는 함수 
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);	//background를 그려줌 
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(background, 0, 0, null);
+		this.repaint();	//paint함수로 돌아감.
 	}
 
 	public static void main(String[] args) {
