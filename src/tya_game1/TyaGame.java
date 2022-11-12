c5b3b923ec90967c8003b2c6139924d6af2a02b9
Insori <s2110@e-mirim.hs.kr>
2022-11-12 23:03:30 +0900

    check

--- src/tya_game1/TyaGame.java
+++ src/tya_game1/TyaGame.java
@@ -10,6 +10,7 @@ import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class TyaGame extends JFrame {
 	private Image screenImage;
@@ -17,9 +18,11 @@ public class TyaGame extends JFrame {
 	private Image background = new ImageIcon(Main.class.getResource("../images/background_title.jpg")).getImage();	//배경이미지
 	private ImageIcon startButtonImage = new ImageIcon(Main.class.getResource("../images/start.png"));	//시작 버튼
 	private ImageIcon howtoplayButtonImage = new ImageIcon(Main.class.getResource("../images/howtoplay.png"));	//방법 버튼
+	private ImageIcon exitButtonImage = new ImageIcon(Main.class.getResource("../images/exit.png"));
 	
 	private JButton startButton = new JButton(startButtonImage);
 	private JButton howtoplayButton = new JButton(howtoplayButtonImage);
+	private JButton exitButton = new JButton(exitButtonImage);
 	
 	public TyaGame() {
 		setUndecorated(true);
@@ -30,22 +33,20 @@ public class TyaGame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로세스도 종료
 		setVisible(true);
 		setBackground(new Color(0, 0, 0, 0));
-		setLayout(null);
+		//setLayout(null);
 		
-		startButton.setBounds(480, 250, 300, 300);
+		startButton.setBounds(480, 230, 217, 67);
 		startButton.setBorderPainted(false);
 		startButton.setContentAreaFilled(false);
 		startButton.setFocusPainted(false);
 		startButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseEntered(MouseEvent e) {	//마우스가 해당 컴포넌트 영역 안으로 들어올때 발생
-				//startButton.setIcon(defaultIcon);	
-				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			public void mouseEntered(MouseEvent e) {
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			@Override
-			public void mouseExited(MouseEvent e) {	//마우스가 해당 컴포넌트 영역 밖으로 나갈때 발생
-				//startButton.setIcon(defaultIcon);	
-				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			public void mouseExited(MouseEvent e) {
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -54,20 +55,18 @@ public class TyaGame extends JFrame {
 		});
 		add(startButton);
 		
-		howtoplayButton.setBounds(430, 320, 400, 400);
+		howtoplayButton.setBounds(430, 260, 400, 69);
 		howtoplayButton.setBorderPainted(false);
 		howtoplayButton.setContentAreaFilled(false);
 		howtoplayButton.setFocusPainted(false);
 		howtoplayButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseEntered(MouseEvent e) {	//마우스가 해당 컴포넌트 영역 안으로 들어올때 발생
-				//howtoplayButton.setIcon(defaultIcon);
-				howtoplayButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			public void mouseEntered(MouseEvent e) {
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			@Override
-			public void mouseExited(MouseEvent e) {	//마우스가 해당 컴포넌트 영역 밖으로 나갈때 발생
-				//howtoplayButton.setIcon(defaultIcon);
-				howtoplayButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			public void mouseExited(MouseEvent e) {
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -75,7 +74,28 @@ public class TyaGame extends JFrame {
 			}
 		});
 		add(howtoplayButton);
+		
+		exitButton.setBounds(480, 200, 149, 67);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		add(exitButton);
 	}
+	
 	
 
 	public void paint(Graphics g) {
