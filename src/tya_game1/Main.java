de72b13bcfb15d16552409bf9277a64f486e6855
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-15 11:19:09 +0900

    배경 크기 변경

--- src/tya_game1/Main.java
+++ src/tya_game1/Main.java
@@ -1,8 +1,8 @@
 package tya_game1;
 
 public class Main {
-	public static final int SCREEN_WIDTH = 1280;
-	public static final int SCREEN_HEIGHT = 720;
+	public static final int SCREEN_WIDTH = 1920;
+	public static final int SCREEN_HEIGHT = 1080;
 	
 	public static void main(String[] args) {
 		new TyaGame();
