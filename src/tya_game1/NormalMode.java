a0683c63e38c6c8f73d7242b519020bad5c100ff
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-22 23:55:25 +0900

    일시정지 버튼 추가

--- src/tya_game1/NormalMode.java
+++ src/tya_game1/NormalMode.java
@@ -8,9 +8,12 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -42,6 +45,28 @@ public class NormalMode extends JFrame {
 	private Image blue_mon = new ImageIcon(Main.class.getResource("../images/BlueMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
 	private Image red_mon = new ImageIcon(Main.class.getResource("../images/RedMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
 	
+	//일시정지버튼 추가
+	private Image stop = new ImageIcon(Main.class.getResource("../images/stop.png")).getImage().getScaledInstance(SCREEN_WIDTH/16, SCREEN_WIDTH/16, 0);
+	private ImageIcon stopButton1 = new ImageIcon(stop);
+	private JButton stopJButton = new JButton(stopButton1);
+	
+	//눌리기 전 버튼 이미지
+	//redButton
+	private Image redButton = new ImageIcon(Main.class.getResource("../images/red_button.png")).getImage();
+	private Image blueButton = new ImageIcon(Main.class.getResource("../images/blue_button.png")).getImage();
+	private Image greenButton = new ImageIcon(Main.class.getResource("../images/green_button.png")).getImage();
+	   
+	//눌린 버튼 이미지
+	private Image redPressedButton = new ImageIcon(Main.class.getResource("../images/pressed_red_button.png")).getImage();
+	private Image bluePressedButton = new ImageIcon(Main.class.getResource("../images/pressed_blue_button.png")).getImage();
+	private Image greenPressedButton = new ImageIcon(Main.class.getResource("../images/pressed_green_button.png")).getImage();
+
+
+	private Image red = redButton;
+	private Image blue = blueButton;
+	private Image green = greenButton;
+	
+	
 	public NormalMode() {
 		setUndecorated(true);
 		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -50,8 +75,74 @@ public class NormalMode extends JFrame {
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
 		
+		//돌아가기 버튼
+		stopJButton.setBounds(SCREEN_WIDTH-SCREEN_WIDTH/13, 0, SCREEN_WIDTH/13, SCREEN_WIDTH/13);
+		stopJButton.setBorderPainted(false);
+		stopJButton.setContentAreaFilled(false);
+		stopJButton.setFocusPainted(false);
+		stopJButton.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					setCursor(new Cursor(Cursor.HAND_CURSOR));
+				}
+				@Override
+				public void mouseExited(MouseEvent e) {
+					setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				}
+				@Override
+				public void mousePressed(MouseEvent e) {
+					new TyaGame();
+				}
+				});
+				add(stopJButton);
 		
+		addKeyListener(new KeyAdapter() {   //키 이벤트
+	         @Override
+	         public void keyPressed(KeyEvent e) {   //키 눌렀을 때
+	            switch (e.getKeyCode()) {
+	            case KeyEvent.VK_LEFT:   //아래 방향키 눌렀을 때
+	               red = redPressedButton;
+	               break;
+	            case KeyEvent.VK_DOWN:   //왼쪽 방향키 눌렀을 때
+	               blue = bluePressedButton;
+	               break;
+	            case KeyEvent.VK_RIGHT:   //오른쪽 방향키 눌렀을 때
+	               green = greenPressedButton;
+	               break;
+	            default:
+	               break;
+	            }
+	         }
+	         public void keyReleased(KeyEvent e) {
+	        	 switch(e.getKeyCode()) {
+	        	 case KeyEvent.VK_LEFT:   //아래 방향키 눌렀을 때
+	        		 red = redButton;
+	        		 break;
+	        	 case KeyEvent.VK_DOWN:   //왼쪽 방향키 눌렀을 때
+	        		 green = greenButton;
+	        		 break;
+	        	 case KeyEvent.VK_RIGHT:   //오른쪽 방향키 눌렀을 때
+	        		 blue = blueButton;
+	        		 break;
+	        	 default:
+	        		 break;
+	        	 }
+	         }
+		});
 	}
+//	
+//	class MyKeyListener extends KeyAdapter {
+//
+//        public void keyPressed(KeyEvent e) {
+//            int keyCode = e.getKeyCode();	// 키 코드 알아내기
+//            
+//        }
+//
+//        public void keyReleased(KeyEvent e) {
+//        	
+//        }
+//
+//    }
 	
 	public void paint(Graphics g) {
 		//그리는 함수 
@@ -60,7 +151,9 @@ public class NormalMode extends JFrame {
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
-		g.drawImage(green_mon, 0, 0, null);
+		g.drawImage(red, 200, 200, null);
+		g.drawImage(green, 400, 400, null);
+		g.drawImage(blue, 600, 600, null);
 	}
 	
 	public void screenDraw(Graphics g) {
