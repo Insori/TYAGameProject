8bcea716b0e5a9c30b78ecb27d704d4d9c8e6209
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-26 14:21:26 +0900

    이미지를 배열 안에 넣기

--- src/tya_game1/NormalMode.java
+++ src/tya_game1/NormalMode.java
@@ -41,9 +41,9 @@ public class NormalMode extends JFrame {
 	private Graphics screenGraphic;
 	
 	private Image background_game = new ImageIcon(Main.class.getResource("../images/background_game.jpg")).getImage().getScaledInstance(SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-	private Image green_mon = new ImageIcon(Main.class.getResource("../images/GreenMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
-	private Image blue_mon = new ImageIcon(Main.class.getResource("../images/BlueMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
-	private Image red_mon = new ImageIcon(Main.class.getResource("../images/RedMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+//	private Image green_mon = new ImageIcon(Main.class.getResource("../images/GreenMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+//	private Image blue_mon = new ImageIcon(Main.class.getResource("../images/BlueMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+//	private Image red_mon = new ImageIcon(Main.class.getResource("../images/RedMon.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
 	
 	//일시정지버튼 추가
 	private Image stop = new ImageIcon(Main.class.getResource("../images/stop.png")).getImage().getScaledInstance(SCREEN_WIDTH/16, SCREEN_WIDTH/16, 0);
@@ -52,20 +52,26 @@ public class NormalMode extends JFrame {
 	
 	//눌리기 전 버튼 이미지
 	//redButton
-	private Image redButton = new ImageIcon(Main.class.getResource("../images/red_button.png")).getImage();
-	private Image blueButton = new ImageIcon(Main.class.getResource("../images/blue_button.png")).getImage();
-	private Image greenButton = new ImageIcon(Main.class.getResource("../images/green_button.png")).getImage();
+	private Image redButton = new ImageIcon(Main.class.getResource("../images/red_button.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+	private Image blueButton = new ImageIcon(Main.class.getResource("../images/blue_button.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+	private Image greenButton = new ImageIcon(Main.class.getResource("../images/green_button.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
 	   
 	//눌린 버튼 이미지
-	private Image redPressedButton = new ImageIcon(Main.class.getResource("../images/pressed_red_button.png")).getImage();
-	private Image bluePressedButton = new ImageIcon(Main.class.getResource("../images/pressed_blue_button.png")).getImage();
-	private Image greenPressedButton = new ImageIcon(Main.class.getResource("../images/pressed_green_button.png")).getImage();
-
-
+	private Image redPressedButton = new ImageIcon(Main.class.getResource("../images/pressed_red_button.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+	private Image bluePressedButton = new ImageIcon(Main.class.getResource("../images/pressed_blue_button.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+	private Image greenPressedButton = new ImageIcon(Main.class.getResource("../images/pressed_green_button.png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+	
 	private Image red = redButton;
 	private Image blue = blueButton;
 	private Image green = greenButton;
 	
+	//몬스터
+	Image[] po = new Image[7];
+	ImageIcon[] position = new ImageIcon[7];
+	Image[] positions = new Image[7];
+	
+	//죽인 몬스터의 수 세기
+	int cnt_monster = 0;
 	
 	public NormalMode() {
 		setUndecorated(true);
@@ -74,6 +80,46 @@ public class NormalMode extends JFrame {
 		setVisible(true);
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
+		setFocusable(true);
+		
+		for(int i = 0; i<positions.length; i++) {
+
+			 po[i] = new ImageIcon(Main.class.getResource("../images/Mon"+(i+1)+".png")).getImage().getScaledInstance(SCREEN_WIDTH/10, SCREEN_WIDTH/10, 0);
+		 }
+		
+		addKeyListener(new KeyAdapter() {   //키 이벤트
+	         @Override
+	         public void keyPressed(KeyEvent e) {   //키 눌렀을 때
+	            switch (e.getKeyCode()) {
+	            case KeyEvent.VK_LEFT:   //아래 방향키 눌렀을 때
+	            	red = redPressedButton;
+	               break;
+	            case KeyEvent.VK_DOWN:   //왼쪽 방향키 눌렀을 때
+	            	green = greenPressedButton;
+	               break;
+	            case KeyEvent.VK_RIGHT:   //오른쪽 방향키 눌렀을 때
+		            blue = bluePressedButton;
+	               break;
+	            default:
+	               break;
+	            }
+	         }
+	         public void keyReleased(KeyEvent e) {
+	        	 switch(e.getKeyCode()) {
+	        	 case KeyEvent.VK_LEFT:   //아래 방향키 눌렀을 때
+	        		 red = redButton;
+	        		 break;
+	        	 case KeyEvent.VK_DOWN:   //왼쪽 방향키 눌렀을 때
+	        		 green = greenButton;
+	        		 break;
+	        	 case KeyEvent.VK_RIGHT:   //오른쪽 방향키 눌렀을 때
+	        		 blue = blueButton;
+	        		 break;
+	        	 default:
+	        		 break;
+	        	 }
+	         }
+		});
 		
 		//돌아가기 버튼
 		stopJButton.setBounds(SCREEN_WIDTH-SCREEN_WIDTH/13, 0, SCREEN_WIDTH/13, SCREEN_WIDTH/13);
@@ -96,53 +142,7 @@ public class NormalMode extends JFrame {
 				});
 				add(stopJButton);
 		
-		addKeyListener(new KeyAdapter() {   //키 이벤트
-	         @Override
-	         public void keyPressed(KeyEvent e) {   //키 눌렀을 때
-	            switch (e.getKeyCode()) {
-	            case KeyEvent.VK_LEFT:   //아래 방향키 눌렀을 때
-	               red = redPressedButton;
-	               break;
-	            case KeyEvent.VK_DOWN:   //왼쪽 방향키 눌렀을 때
-	               blue = bluePressedButton;
-	               break;
-	            case KeyEvent.VK_RIGHT:   //오른쪽 방향키 눌렀을 때
-	               green = greenPressedButton;
-	               break;
-	            default:
-	               break;
-	            }
-	         }
-	         public void keyReleased(KeyEvent e) {
-	        	 switch(e.getKeyCode()) {
-	        	 case KeyEvent.VK_LEFT:   //아래 방향키 눌렀을 때
-	        		 red = redButton;
-	        		 break;
-	        	 case KeyEvent.VK_DOWN:   //왼쪽 방향키 눌렀을 때
-	        		 green = greenButton;
-	        		 break;
-	        	 case KeyEvent.VK_RIGHT:   //오른쪽 방향키 눌렀을 때
-	        		 blue = blueButton;
-	        		 break;
-	        	 default:
-	        		 break;
-	        	 }
-	         }
-		});
 	}
-//	
-//	class MyKeyListener extends KeyAdapter {
-//
-//        public void keyPressed(KeyEvent e) {
-//            int keyCode = e.getKeyCode();	// 키 코드 알아내기
-//            
-//        }
-//
-//        public void keyReleased(KeyEvent e) {
-//        	
-//        }
-//
-//    }
 	
 	public void paint(Graphics g) {
 		//그리는 함수 
@@ -151,9 +151,12 @@ public class NormalMode extends JFrame {
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
-		g.drawImage(red, 200, 200, null);
-		g.drawImage(green, 400, 400, null);
-		g.drawImage(blue, 600, 600, null);
+		g.drawImage(red, SCREEN_WIDTH/10, SCREEN_HEIGHT-SCREEN_HEIGHT/7, null);
+		g.drawImage(green, SCREEN_WIDTH/2-SCREEN_WIDTH/19, SCREEN_HEIGHT-SCREEN_HEIGHT/7, null);
+		g.drawImage(blue, SCREEN_WIDTH-SCREEN_WIDTH/5, SCREEN_HEIGHT-SCREEN_HEIGHT/7, null);
+		for(int i = 0; i<7; i++) {
+			g.drawImage(po[i], SCREEN_WIDTH/(i+1), SCREEN_HEIGHT/(i+6), null);
+		}
 	}
 	
 	public void screenDraw(Graphics g) {
